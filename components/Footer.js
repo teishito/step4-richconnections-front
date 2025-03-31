e3ded5d260dff753c05b2dd04c08fbc282ea551c
teishito <z.tengqyding@gmail.com>
2025-03-31 23:21:03 +0900

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="fixed bottom-0 left-0 w-full bg-gray-200 py-4 text-center shadow-md">
-            <p className="text-sm text-gray-500">© 2024 FortuneDinner. All rights reserved.</p>
+            <p className="text-sm text-gray-500">© 2025 rich connections. All rights reserved.</p>
         </footer>
     );
 };
