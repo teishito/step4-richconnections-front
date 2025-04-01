17132b286d909f0511080109fe9e1c5a22945e9b
teishito <z.tengqyding@gmail.com>
2025-04-01 09:08:58 +0900

    Create login.js

---
+++ pages/login.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Login from "../components/Login";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Login />
+    </div>
+  );
+}
