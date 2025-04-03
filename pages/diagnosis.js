5a0fbd2c682fb9109e3b3f34549aac7843ee301b
teishito <z.tengqyding@gmail.com>
2025-04-03 23:42:19 +0900

    Update diagnosis.js

--- pages/diagnosis.js
+++ pages/diagnosis.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import SearchForm from "../components/SearchForm";
+import SearchForm from "../components/Diagnosis";
 
 export default function DiagnosisPage() {
   return (
