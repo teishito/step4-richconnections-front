dec52bfcd433017ee4d72bdf2074ed80af2b02a0
teishito <z.tengqyding@gmail.com>
2025-04-04 00:09:25 +0900

    Update diagnosis.js

--- pages/diagnosis.js
+++ pages/diagnosis.js
@@ -1,13 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import SearchForm from "../components/Diagnosis";
+import SearchForm from "../components/SearchForm";
 
 export default function DiagnosisPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
       <main className="flex-grow flex flex-col items-center p-4">
-        <Diagnosis />
+        <SearchForm />
       </main>
       <Footer />
     </div>
