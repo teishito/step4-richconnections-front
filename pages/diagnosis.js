2acc62705b2e7b1cdf065269197372075035ddf0
teishito <z.tengqyding@gmail.com>
2025-04-03 23:36:08 +0900

    Update diagnosis.js

--- pages/diagnosis.js
+++ pages/diagnosis.js
@@ -7,7 +7,7 @@ export default function DiagnosisPage() {
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
       <main className="flex-grow flex flex-col items-center p-4">
-        <SearchForm />
+        <Diagnosis />
       </main>
       <Footer />
     </div>
