e5f2e39081ea0ac50933e840644e21215e252b65
teishito <z.tengqyding@gmail.com>
2025-04-07 00:14:06 +0900

    Update diagnosis-analysis.js

--- pages/diagnosis-analysis.js
+++ pages/diagnosis-analysis.js
@@ -6,7 +6,7 @@ export default function DiagnosisAnalysisPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       <Header />
-      <main className="flex-grow flex flex-col items-center p-4">
+      <main className="flex-grow flex flex-col items-center p-4 pb-40">
         <DiagnosisAnalysis />
       </main>
       <Footer />
