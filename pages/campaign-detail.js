ef409df31178cd94519886856369a07863b54484
teishito <z.tengqyding@gmail.com>
2025-04-09 22:42:37 +0900

    Create campaign-detail.js

---
+++ pages/campaign-detail.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import CampaignDetail from "../components/CampaignDetail";
+
+export default function CampaignDetailPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center p-4">
+      <CampaignDetail />
+    </div>
+  );
+}
