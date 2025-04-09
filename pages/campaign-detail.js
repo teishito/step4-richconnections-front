ab567751c73a0397c627c5d7aeb1085826d1e5e2
teishito <z.tengqyding@gmail.com>
2025-04-09 22:54:53 +0900

    Update campaign-detail.js

--- pages/campaign-detail.js
+++ pages/campaign-detail.js
@@ -4,8 +4,12 @@ import CampaignDetail from "../components/CampaignDetail";
 
 export default function CampaignDetailPage() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center p-4">
-      <CampaignDetail />
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Header />
+      <main className="flex-grow flex flex-col items-center p-4 pb-40">
+        <CampaignDetail />
+      </main>
+      <Footer />
     </div>
   );
 }
