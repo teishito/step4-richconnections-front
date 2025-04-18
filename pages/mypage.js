ee12d12ac753ce5f23a6bca20d5d3f3464709a1e
teishito <z.tengqyding@gmail.com>
2025-04-19 07:58:18 +0900

    Update mypage.js

--- pages/mypage.js
+++ pages/mypage.js
@@ -1,13 +1,13 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import MypageForm from "../components/MypageForm";
+import Mypage from "../components/MyPage";
 
 export default function Mypage() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
       <main className="flex-grow p-6 flex justify-center items-start">
-        <MypageForm />
+        <Mypage />
       </main>
       <Footer />
     </div>
