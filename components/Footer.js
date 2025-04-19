e0f7f8a47e4796f98903c5f56698f0acc2e56728
teishito <z.tengqyding@gmail.com>
2025-04-19 14:01:57 +0900

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,34 @@
-const Footer = () => {
-    return (
-        <footer className="fixed bottom-0 left-0 w-full bg-gray-200 py-4 text-center shadow-md">
-            <p className="text-sm text-gray-500">© 2025 rich connections. All rights reserved.</p>
-        </footer>
-    );
-};
+import Link from "next/link";
+import { FaClipboardList, FaPlay, FaCheckCircle, FaUserCircle } from "react-icons/fa";
 
-export default Footer;
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 shadow z-50">
+      <div className="flex justify-between items-center px-6 py-2 text-sm text-[#5B7F6F]">
+        {/* Plan */}
+        <Link href="/sns-campaign" className="flex flex-col items-center">
+          <FaClipboardList className="text-lg" />
+          <span className="text-xs">Plan</span>
+        </Link>
+
+        {/* Do */}
+        <Link href="/campaign-detail" className="flex flex-col items-center">
+          <FaPlay className="text-lg" />
+          <span className="text-xs">Do</span>
+        </Link>
+
+        {/* Check */}
+        <Link href="/campaign-report" className="flex flex-col items-center">
+          <FaCheckCircle className="text-lg" />
+          <span className="text-xs">Check</span>
+        </Link>
+
+        {/* MyPage */}
+        <Link href="/mypage" className="flex flex-col items-center">
+          <FaUserCircle className="text-lg" />
+          <span className="text-xs">MyPage</span>
+        </Link>
+      </div>
+    </footer>
+  );
+}
